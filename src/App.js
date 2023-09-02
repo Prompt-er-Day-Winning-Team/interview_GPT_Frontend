3bc917360c3686b2d86e527fb66675a7b004a503
dan-2ee <ekdms7273@naver.com>
2023-09-02 17:20:13 +0900

    feat: join page routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/login";
+import JoinPage from "./pages/join";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/loginpage" element={<LoginPage />} />
+        <Route path="/joinpage" element={<JoinPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/join/index.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Header from "../../components/common/Header";
+import * as S from "./style";
+
+function JoinPage() {
+  return (
+    <S.Wrap>
+      <Header />
+    </S.Wrap>
+  );
+}
+
+export default JoinPage;
