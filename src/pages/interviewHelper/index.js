d1dded80317d6611a26631763035ff7c0a546823
smhwang0109 <smhwang0109@gmail.com>
2023-09-03 19:26:46 +0900

    fix: interview-helper

--- src/pages/interviewHelper/index.js
+++ src/pages/interviewHelper/index.js
@@ -1,44 +1,82 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import * as S from "./style";
+  
+const textToSpeak = [
+  "1124124",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+];
 
 function InterviewHelper() {
-  const textToSpeak = "에이닷 사용하면서 불편한 점은 어떤게 있으셨나요?";
+  const [isRecord, setIsRecord] = useState(true)
+  const [textToSpeakIdx, setTextToSpeakIdx] = useState(-1)
+
+  const handleInputChange = (e) => {
+    if (isRecord) {
+      setIsRecord(!isRecord)
+      setTextToSpeakIdx(textToSpeakIdx + 1)
+      speakText(textToSpeak[textToSpeakIdx + 1])
+    } else {
+      setIsRecord(!isRecord)
+    }
+  };
+
+  // useEffect(() => {
+  //   speakText(textToSpeak[textToSpeakIdx])
+  // }, []);
 
   const speakText = (text) => {
+    const lang = "ko-KR"
     const synth = window.speechSynthesis;
     if (synth && text) {
       const utterance = new SpeechSynthesisUtterance(text);
+      utterance.lang = lang
+      utterance.pitch = 1
+      utterance.rate = 0.9
       synth.speak(utterance);
     }
   };
 
-  useEffect(() => {
-    speakText(textToSpeak);
-
-    return () => {
-      const synth = window.speechSynthesis;
-      if (synth && synth.speaking) {
-        synth.cancel();
-      }
-    };
-  }, []);
-
   return (
     <S.Wrap>
-      <S.Title onClick={speakText}>
-        {"에이닷 사용하면서 불편한 점은 어떤게 있으셨나요?"}
+      <S.Title>
+        {textToSpeak[textToSpeakIdx]}
       </S.Title>
       <img
         src={process.env.PUBLIC_URL + "/images/Record/logo.gif"}
         alt="gif"
         style={{ width: "350px", height: "350px" }}
       />
-      <img
-        src={process.env.PUBLIC_URL + "/images/Record/recordButton.svg"}
-        alt="record"
-        style={{ width: "150px", height: "50px", cursor: "pointer" }}
-        onClick={speakText}
-      />
+      {console.log(isRecord)}
+      {isRecord ? 
+      <>
+        {textToSpeakIdx === -1 ? 
+          <img
+            src={process.env.PUBLIC_URL + "/images/Record/startButton.svg"}
+            alt="start"
+            style={{ width: "150px", height: "50px", cursor: "pointer" }}
+            onClick={handleInputChange}
+          />
+        :
+        <img
+          src={process.env.PUBLIC_URL + "/images/Record/stopButton.svg"}
+          alt="stop"
+          style={{ width: "150px", height: "50px", cursor: "pointer" }}
+          onClick={handleInputChange}
+        />
+        }
+      </>
+      :
+        <img
+          src={process.env.PUBLIC_URL + "/images/Record/recordButton.svg"}
+          alt="record"
+          style={{ width: "150px", height: "50px", cursor: "pointer" }}
+          onClick={handleInputChange}
+        />
+      }
     </S.Wrap>
   );
 }
