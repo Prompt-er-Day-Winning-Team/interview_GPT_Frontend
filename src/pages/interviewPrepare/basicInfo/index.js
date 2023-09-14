640eb2a0907a8e3e66aee0b382cbff8b509bf0ed
dan-2ee <ekdms7273@naver.com>
2023-09-14 20:24:07 +0900

    feat: basicInfo api call

--- src/pages/interviewPrepare/basicInfo/index.js
+++ src/pages/interviewPrepare/basicInfo/index.js
@@ -1,20 +1,38 @@
 import React, { useState } from "react";
 import Header from "../../../components/common/Header";
 import * as S from "./style";
-import { ReactComponent as IdeaIcon } from "../../../asset/icons/status-idea.svg"
-import { ReactComponent as ConceptIcon } from "../../../asset/icons/status-concept.svg"
-import { ReactComponent as PrototypeIcon } from "../../../asset/icons/status-prototype.svg"
-import { ReactComponent as ReleaseIcon } from "../../../asset/icons/status-release.svg"
+import { ReactComponent as IdeaIcon } from "../../../asset/icons/status-idea.svg";
+import { ReactComponent as ConceptIcon } from "../../../asset/icons/status-concept.svg";
+import { ReactComponent as PrototypeIcon } from "../../../asset/icons/status-prototype.svg";
+import { ReactComponent as ReleaseIcon } from "../../../asset/icons/status-release.svg";
 import { TextInput } from "../../../components/common/Input";
 import Button from "../../../components/common/Button";
 import { useNavigate } from "react-router-dom";
+import { convertCreateInterviewUrl } from "../../../utils/apis";
+import axios from "axios";
 
 const statusList = [
-  [<IdeaIcon/>, "아이디어", "초기 단계로 진출하고자 하는 시장과 해당 시장의 소비자를 이해하는 단계입니다."],
-  [<ConceptIcon/>, "컨셉 기획", "아이디어를 바탕으로 제품의 컨셉을 기획하는 단계입니다."],
-  [<PrototypeIcon/>, "프로토타입", "컨셉을 구체화한 후 만든 프로토타입을 기준으로, 개선이 필요한 사항을 발견합니다."],
-  [<ReleaseIcon/>, "출시", "의도한 대로 제품을 사용하고 있는지를 확인하고, 테스트를 위한 리서치를 시작합니다."]
-]
+  [
+    <IdeaIcon />,
+    "아이디어",
+    "초기 단계로 진출하고자 하는 시장과 해당 시장의 소비자를 이해하는 단계입니다.",
+  ],
+  [
+    <ConceptIcon />,
+    "컨셉 기획",
+    "아이디어를 바탕으로 제품의 컨셉을 기획하는 단계입니다.",
+  ],
+  [
+    <PrototypeIcon />,
+    "프로토타입",
+    "컨셉을 구체화한 후 만든 프로토타입을 기준으로, 개선이 필요한 사항을 발견합니다.",
+  ],
+  [
+    <ReleaseIcon />,
+    "출시",
+    "의도한 대로 제품을 사용하고 있는지를 확인하고, 테스트를 위한 리서치를 시작합니다.",
+  ],
+];
 
 function BasicInfo() {
   const navigate = useNavigate();
@@ -34,6 +52,33 @@ function BasicInfo() {
       [name]: value,
     });
   };
+
+  const handleNextButton = () => {
+    var userId = localStorage.getItem("user_id");
+
+    const response = axios
+      .post(
+        convertCreateInterviewUrl(userId),
+        {
+          interviewGoal: productForm.goal,
+          productDetail: productForm.productDetail,
+          productName: productForm.productName,
+          status: selectedStatus,
+          targetUser: productForm.targetUser,
+        },
+        {
+          headers: {
+            accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then(function (response) {
+        localStorage.setItem("interview_id", response.data?.interviewId);
+        navigate("/prepare/persona");
+      })
+      .catch(function (error) {});
+  };
   return (
     <S.Wrap>
       <Header />
@@ -43,7 +88,8 @@ function BasicInfo() {
           {statusList.map((status, idx) => (
             <S.Card
               selected={selectedStatus === idx}
-              onClick={() => (setSelectedStatus(idx))}>
+              onClick={() => setSelectedStatus(idx)}
+            >
               {status[0]}
               <S.CardTitle>{status[1]}</S.CardTitle>
               <S.CardContents>{status[2]}</S.CardContents>
@@ -95,7 +141,14 @@ function BasicInfo() {
         />
         <S.TextCount>{`${productForm.targetUser.length}/100`}</S.TextCount>
         <S.ButtonContainer>
-          <Button text={"다음으로"} width={"192px"} height={"44px"} backgroundColor={"#333335"} color={"#F1F4F9"} onClick={() => navigate("/prepare/persona")} />
+          <Button
+            text={"다음으로"}
+            width={"192px"}
+            height={"44px"}
+            backgroundColor={"#333335"}
+            color={"#F1F4F9"}
+            onClick={handleNextButton}
+          />
         </S.ButtonContainer>
       </S.InterviewQuestionBlock>
     </S.Wrap>
--- src/utils/apis.js
+++ src/utils/apis.js
@@ -5,3 +5,7 @@ export const convertRegistUserUrl = () => {
 export const convertLoginUrl = () => {
   return "https://i-dot-gpt-81f182021aa0.herokuapp.com/v1/login";
 };
+
+export const convertCreateInterviewUrl = (user_id) => {
+  return `https://i-dot-gpt-81f182021aa0.herokuapp.com/v1/users/${user_id}/prepare/interviews`;
+};
